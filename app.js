7d138c036403dd8f9d50a9dc658687f1347730ce
iftikhar chetoui <115534833+foufa102022@users.noreply.github.com>
2023-05-20 00:08:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');  //appel d'express
-const puppeteer = require ('puppeteer');        //appel du puppeteer
-const engine = require ('express-handlebars').engine;       //appel module engine de l'exress
-const app = express();             //declaration de variable
-app.use(express.static('public'))       //appel de dossier public
-app.engine('handlebars',engine());      //
-app.set('view engine' , 'handlebars');            // 
+const express = require('express');  
+const puppeteer = require ('puppeteer');        
+const engine = require ('express-handlebars').engine;       
+const app = express();             
+app.use(express.static('public'))     
+app.engine('handlebars',engine());     
+app.set('view engine' , 'handlebars');            
 app.set('views','./views');
 
 
@@ -15,7 +15,7 @@ app.get('/', (req, res) =>
 app.get ('/search',(req, res)=>
 {
     let searchQuery= req.query.q;
-    let searchamazonURL = 'https://www.wiki.tn/c/' + searchQuery ;
+    let searwikiURL = 'https://www.wiki.tn/c/' + searchQuery ;
     console.log (searchQuery);
 
     res.render ('search');
